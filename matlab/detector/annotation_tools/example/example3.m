f1a2e3860d4a25014b54f9fa8a139410ab4b6bb7
Ning Zhang <nzhang@eecs.berkeley.edu>
2014-12-19 17:21:10 -0800

    Ning remove unnessary files

--- matlab/detector/annotation_tools/example/example3.m
+++
@@ -1,9 +0,0 @@
-function example3(a)
-
-disp(sprintf('Press \''<-\'' and \''->\'' to navigate the H3D data set'));
-disp(sprintf('Press \''g\'' to go to a given annotation. '));
-disp(sprintf('Press \''SPACE\'' and drag a rectangle over a body part of a person to find similar patches'));
-disp(sprintf('See the comments inside browse_annotations.m for additional commands'));
-disp(sprintf('Press \''ESC\'' to exit. '));
-
-browse_annotations(a);
